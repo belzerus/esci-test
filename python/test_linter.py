7f4ebde6912528c3683bac34f5918e4c502af4c9
Petter Mabacker <petter.mabacker@esab.se>
2022-05-12 14:54:39 +0200

    Initial commit

---
+++ python/test_linter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def print_some_info():
+    str = "a" + "b" + "c"
+    print(str)
+
+
+def main():
+    """Main func"""
+    print_some_info()
+
+
+if __name__ == "__main__":
+    main()
